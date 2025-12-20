f71c407ce695539456f7b992c30de44d8b40b2ec
skadnan40605 <driveronhireadnan@gmail.com>
2025-12-20 10:02:55 +0530

    Configure REST Framework to allow API access without CSRF

--- config/settings.py
+++ config/settings.py
@@ -146,6 +146,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
 }
 
 # CORS Configuration
